dae5df97a6757ad54ffd4145ef26a8cd0a817ff4
chloroplast <41893204@qq.com>
2016-09-11 02:25:31 +0800

    添加默认config

--- Application/config.php
+++ Application/config.php
@@ -1,3 +1,10 @@
 <?php
-
-define('ANIMALS', array('bb'=>'cc','aa'=>'dd'));
+//通用
+/**
+ * @var int STATUS_NORMAL 默认状态
+ */
+define('STATUS_NORMAL', 0);
+/**
+ * @var int STATUS_DELETE 删除状态
+ */
+define('STATUS_DELETE', -2);
