ae68f29e294b7e22340cd6a056db0530c27db003
chloroplast <41893204@qq.com>
2016-09-20 14:11:17 +0800

    更新 userController

--- Application/User/Controller/UserController.class.php
+++ Application/User/Controller/UserController.class.php
@@ -11,29 +11,7 @@ class UserController extends Controller
     
     public function get(string $ids = '')
     {
-        //http://www.marmot.com/users?filter[test]=1&filter[aa]=2&sort=-created,title&include=comments.author&fields[articles]=title,body&page[num]=1&page[size]=10
-        echo '<pre>';
-        if (!empty($ids)) {
-            if (is_numeric($ids)) {//获取单条
-                var_dump('single:'.$ids);
-            } else {//批量获取
-                var_dump('multi:'.$ids);
-            }
-        } else {//查询
-            $parameters = $this->getParameters();
-            var_dump('filter:');
-            var_dump($parameters);
-        }
-
-        // $view = new NewsView([$news, $news2]);
-        // $this->render($view->pagination(
-        //         $url = 'users',
-        //         $conditions = $this->getRequest()->get(),
-        //         $num = 50,
-        //         $perpage = 10,
-        //         $curpage = 2
-        //     )
-        // );
-        return false;
+        var_dump("user controller");
+        return true;
     }
 }
