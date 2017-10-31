0e9e71428fa9665bb3ff2c928ae3bd4e0ce28c68
chloroplast1983 <41893204@qq.com>
2017-10-31 17:05:51 +0800

    CommandBus ut

--- tests/UnitTest/System/Classes/CommandBusTest.php
+++ tests/UnitTest/System/Classes/CommandBusTest.php
@@ -8,6 +8,7 @@ use System\Classes\CommandBus;
 use System\Interfaces\ICommandHandlerFactory;
 use System\Interfaces\ICommand;
 use System\Interfaces\INull;
+use System\Interfaces\ICommandHandler;
 use System\Classes\NullCommandHandler;
 
 use Prophecy\Argument;
@@ -34,6 +35,88 @@ class CommandBusTest extends GenericTestCase
         unset($this->transaction);
     }
     
+    /**
+     * 1. getTransaction() 需要调用一次
+     * 2. getCommandHandlerFactory() 需要调用一次
+     * 3. transaction->beginTransaction() 调用一次
+     * 4. commandHandher->execute() 假设调用失败
+     * 5. transaction->commit() 不调用
+     * 6. transaction->rollBack() 调用一次
+     */
+    public function testCommandExcuteFailure()
+    {
+        $this->transaction->beginTransaction()->shouldBeCalledTimes(1)->willReturn(true);
+        $this->transaction->commit()->shouldNotBeCalled()->willReturn(false);
+        $this->transaction->rollBack()->shouldBeCalledTimes(1);
+
+        $commandHandler = $this->prophesize(ICommandHandler::class);
+        $commandHandler->execute(
+            Argument::exact($this->command)
+        )->shouldBeCalledTimes(1)->willReturn(false);
+
+        $this->commandHandlerFactory->getHandler(
+            Argument::exact($this->command)
+        )->shouldBeCalledTimes(1)
+         ->willReturn($commandHandler->reveal());
+
+        $this->commandBus= $this->getMockBuilder(CommandBus::class)
+                                ->setMethods(['getTransaction', 'getCommandHandlerFactory'])
+                                ->setConstructorArgs([$this->commandHandlerFactory->reveal()])
+                                ->getMock();
+        $this->commandBus->expects($this->once())
+                         ->method('getCommandHandlerFactory')
+                         ->willReturn($this->commandHandlerFactory->reveal());
+        $this->commandBus->expects($this->once())
+                         ->method('getTransaction')
+                         ->willReturn($this->transaction->reveal());
+        
+        $result = $this->commandBus->send($this->command->reveal());
+
+        $this->assertFalse($result);
+        $this->assertEquals(ERROR_NOT_DEFINED, Core::getLastError()->getId());
+    }
+
+    /**
+     * 1. getTransaction() 需要调用一次
+     * 2. getCommandHandlerFactory() 需要调用一次
+     * 3. transaction->beginTransaction() 调用一次
+     * 4. commandHandher->execute() 调用一次成功
+     * 5. transaction->commit() 调用一次, 假设失败
+     * 6. transaction->rollBack() 调用一次
+     */
+    public function testCommitFailure()
+    {
+        $this->transaction->beginTransaction()->shouldBeCalledTimes(1)->willReturn(true);
+        $this->transaction->commit()->shouldBeCalledTimes(1)->willReturn(false);
+        $this->transaction->rollBack()->shouldBeCalledTimes(1)->willReturn(true);
+
+        $commandHandler = $this->prophesize(ICommandHandler::class);
+        $commandHandler->execute(
+            Argument::exact($this->command)
+        )->shouldBeCalledTimes(1)->willReturn(true);
+
+        $this->commandHandlerFactory->getHandler(
+            Argument::exact($this->command)
+        )->shouldBeCalledTimes(1)
+         ->willReturn($commandHandler->reveal());
+
+        $this->commandBus= $this->getMockBuilder(CommandBus::class)
+                                ->setMethods(['getTransaction', 'getCommandHandlerFactory'])
+                                ->setConstructorArgs([$this->commandHandlerFactory->reveal()])
+                                ->getMock();
+        $this->commandBus->expects($this->once())
+                         ->method('getCommandHandlerFactory')
+                         ->willReturn($this->commandHandlerFactory->reveal());
+        $this->commandBus->expects($this->once())
+                         ->method('getTransaction')
+                         ->willReturn($this->transaction->reveal());
+        
+        $result = $this->commandBus->send($this->command->reveal());
+
+        $this->assertFalse($result);
+        $this->assertEquals(ERROR_NOT_DEFINED, Core::getLastError()->getId());
+    }
+
     public function testNullCommandHandler()
     {
         $commandHandler = $this->getMockBuilder(NullCommandHandler::class)
@@ -61,15 +144,44 @@ class CommandBusTest extends GenericTestCase
         $this->assertEquals(COMMAND_HANDLER_NOT_EXIST, Core::getLastError()->getId());
     }
 
-    public function testCommandExcuteFailure()
-    {
-    }
-
-    public function testCommitFailure()
-    {
-    }
-
+    /**
+     * 1. 调用getTransaction()一次
+     * 2. 调用getCommandHandlerFactory()一次
+     * 3. 调用transaction->beginTransaction() 一次
+     * 4. 调用transaction->commit() 一次, 返回true
+     * 6. 调用commandHander->execute() 一次,返回true
+     * 7. 调用transaction->rollBack() 不调用
+     * 8. 期望结果返回true
+     */
     public function testSendSuccess()
     {
+        $this->transaction->beginTransaction()->shouldBeCalledTimes(1)->willReturn(true);
+        $this->transaction->commit()->shouldBeCalledTimes(1)->willReturn(true);
+        $this->transaction->rollBack()->shouldNotBeCalled();
+
+        $commandHandler = $this->prophesize(ICommandHandler::class);
+        $commandHandler->execute(
+            Argument::exact($this->command)
+        )->shouldBeCalledTimes(1)->willReturn(true);
+
+        $this->commandHandlerFactory->getHandler(
+            Argument::exact($this->command)
+        )->shouldBeCalledTimes(1)
+         ->willReturn($commandHandler->reveal());
+
+        $this->commandBus= $this->getMockBuilder(CommandBus::class)
+                                ->setMethods(['getTransaction', 'getCommandHandlerFactory'])
+                                ->setConstructorArgs([$this->commandHandlerFactory->reveal()])
+                                ->getMock();
+        $this->commandBus->expects($this->once())
+                         ->method('getCommandHandlerFactory')
+                         ->willReturn($this->commandHandlerFactory->reveal());
+        $this->commandBus->expects($this->once())
+                         ->method('getTransaction')
+                         ->willReturn($this->transaction->reveal());
+        
+        $result = $this->commandBus->send($this->command->reveal());
+        
+        $this->assertTrue($result);
     }
 }
